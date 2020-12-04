b99aa2bf01414796b7204c570aeb5854db6ea503
mboti2000 <mboti20000@gmail.com>
2020-12-04 10:22:46 +0100

    xml load + buttons commit

--- IRF_Project/IRF_Project/Form1.Designer.cs
+++ IRF_Project/IRF_Project/Form1.Designer.cs
@@ -29,13 +29,66 @@ namespace IRF_Project
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 93);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(2, 322);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(997, 297);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1084, 304);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Termék lista letöltése";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(998, 619);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- IRF_Project/IRF_Project/Form1.cs
+++ IRF_Project/IRF_Project/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_Project
 {
     public partial class Form1 : Form
     {
+        XmlDocument xml = new XmlDocument();
         public Form1()
         {
             InitializeComponent();
+            xml.Load(@"car.xml");
+
+            var i = 0;
+            var j = 1;
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                Car c = new Car();
+                CarButton b = new CarButton();
+                c.Model = ((XmlElement)element.ChildNodes[0]).InnerText;
+                c.Photo = ((XmlElement)element.ChildNodes[3]).InnerText;
+                b.pc = pictureBox1;
+                b.img = c.Photo;
+                b.Text = c.Model;
+
+                if (i < 10)
+                {
+                    b.Left = b.Width * i;
+                    i++;
+                }
+                else if (j < 10)
+                {
+                    b.Top = b.Height;
+                    b.Left = b.Width * (j - 1);
+                    j++;
+                }
+
+                panel1.Controls.Add(b);
+            }
         }
     }
 }
