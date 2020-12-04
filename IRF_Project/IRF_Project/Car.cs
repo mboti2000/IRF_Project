0c005b65b529bd2281b1cee7a4a060e068e60db7
mboti2000 <mboti20000@gmail.com>
2020-12-04 09:53:57 +0100

    Car class commit

---
+++ IRF_Project/IRF_Project/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public Color Color { get; set; }
+        public string Photo { get; set; }
+        public int Price { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
